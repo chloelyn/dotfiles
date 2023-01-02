66bd1c4e800317bf4fc7489672298b31b7bb9781
Alaina Newmark <68250402+alaidriel@users.noreply.github.com>
2023-01-02 15:43:00 -0600

    feat: switch nix configuration to home-manager

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -20,6 +20,12 @@
     ./session.nix
   ];
 
+  # Nix
+  nix.package = pkgs.nix;
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
