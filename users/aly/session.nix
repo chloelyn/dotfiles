244044d211da1af6c2d8b02ac8fbaf1bbae287e0
Alaina <68250402+alaidriel@users.noreply.github.com>
2023-01-08 16:30:19 -0600

    chore: remove wakatime

--- users/aly/session.nix
+++ users/aly/session.nix
@@ -12,7 +12,6 @@
     NIX_PATH = "${config.home.homeDirectory}/.nix-defexpr/channels:/nix/var/nix/profiles/per-user/root/channels";
     EDITOR = "/etc/profiles/per-user/aly/bin/hx";
     ZSH_COMPDUMP = "${config.xdg.dataHome}/zsh/zcompdump";
-    WAKATIME_HOME = "${config.home.homeDirectory}/.config/wakatime";
     CARGO_HOME = "${config.xdg.dataHome}/cargo";
     LESSHISTFILE = "-";
   };
